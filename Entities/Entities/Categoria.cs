2b7bda01428f06260057b5f9bd223410271093ca
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-10-13 12:35:18 -0300

    Criar entidades da pasta Entities e a classe Notifica.

---
+++ Entities/Entities/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Entities
+{
+    [Table("Categoria")]
+    public class Categoria : Base
+    {
+        [ForeignKey("SistemaFinanceiro")]
+        [Column(Order = 1)]
+        public int SistemaId { get; set; }
+        // EF Relation
+        public virtual SistemaFinanceiro SistemaFinanceiro { get; set; }
+    }
+}
---
+++ Entities/Notifications/Notifica.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Notifications
+{
+    public class Notifica
+    {
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string Mensagem { get; set; }
+
+        [NotMapped]
+        public IList<Notifica> notificacoes;
+
+        public Notifica()
+        {
+            notificacoes = new List<Notifica>();
+        }
+
+        public bool ValidarPropriedadeString(string valor, string nomePropriedade)
+        {
+            if (String.IsNullOrWhiteSpace(valor) || String.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                notificacoes.Add(new Notifica
+                {
+                    Mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade,
+                });
+
+                return false;
+
+            }
+
+            return true;
+        }
+
+        public bool ValidarPropriedadeInt(int valor, string nomePropriedade)
+        {
+            if (valor < 1 || String.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                notificacoes.Add(new Notifica
+                {
+                    Mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade,
+                });
+
+                return false;
+
+            }
+
+            return true;
+        }
+    }
+}
