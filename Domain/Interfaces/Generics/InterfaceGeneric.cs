8ef422c77fd8d7bf4d2cf3e21b18fc5a312c63c9
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-10-15 12:28:40 -0300

    Criar a classe Generica de Repositório.

--- Domain/Interfaces/Generics/InterfaceGeneric.cs
+++ Domain/Interfaces/Generics/InterfaceGeneric.cs
@@ -1,11 +1,11 @@
 ﻿namespace Domain.Interfaces.Generics
 {
-    public interface InterfaceGeneric<T> where T : class
+    public interface InterfaceGeneric<T> : IDisposable where T : class
     {
         Task<T> GetEntityById(int id);
         Task<IList<T>> List();
-        Task<IEnumerable<T>> Search(Func<T, bool> predicate); // Metodo para buscar qualquer entidade por qualquer parametro, 
-                                                              // retorno uma List Enumerable de obj Entity
+        IEnumerable<T> Search(Func<T, bool> predicate); // Metodo para buscar qualquer entidade por qualquer parametro, 
+                                                        // retorno uma List Enumerable de obj Entity
         Task Add(T entity);
         Task Update(T entity);
         Task Delete(T entity);
---
+++ Infrastructure/Repository/Generics/RepositoryGenerics.cs
@@ -0,0 +1,77 @@
+﻿using Domain.Interfaces.Generics;
+using Infrastructure.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
+using System.Runtime.InteropServices;
+
+namespace Infrastructure.Repository.Generics
+{
+    public abstract class RepositoryGenerics<T> : InterfaceGeneric<T> where T : class
+    {
+        protected readonly ContextBase Db;
+        protected readonly DbSet<T> DbSet;
+
+        protected RepositoryGenerics(ContextBase context)
+        {
+            Db = context;
+            DbSet = context.Set<T>();
+        }
+
+        public async Task<T> GetEntityById(int id) => await DbSet.FindAsync(id);
+
+        public async Task<IList<T>> List() => await DbSet.AsNoTracking().ToListAsync();
+
+        public IEnumerable<T> Search(Func<T, bool> predicate) => DbSet.AsNoTracking().Where(predicate).ToList();
+
+        public async Task Add(T entity)
+        {
+            await DbSet.AddAsync(entity);
+            await Db.SaveChangesAsync();
+        }
+
+        public async Task Delete(T entity)
+        {
+            DbSet.Remove(entity);
+            await Db.SaveChangesAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            DbSet.Update(entity);
+            await Db.SaveChangesAsync();
+        }
+
+        #region Disposed https://docs.microsoft.com/pt-br/dotnet/standard/garbage-collection/implementing-dispose
+        // Flag: Has Dispose already been called?
+        bool disposed = false;
+        // Instantiate a SafeHandle instance.
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                handle.Dispose();
+                // Free any other managed objects here.
+                //
+            }
+
+            disposed = true;
+        }
+        #endregion
+    }
+}
